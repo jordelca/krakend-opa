b8d1a1ed191f4e8a59129a3ed562ea3e037a9220
Jorge Delgado Carrasco <jorge.delgado@scalefast.com>
2021-05-20 13:15:25 +0200

    Middleware

--- opa.go
+++ opa.go
@@ -1,16 +1,39 @@
 package opa
 
 import (
-	"io"
-	"log"
+	"errors"
+	"math/rand"
+	"net/http"
 
 	"github.com/devopsfaith/krakend/config"
+	"github.com/devopsfaith/krakend/logging"
+	"github.com/devopsfaith/krakend/proxy"
+	ginkrakend "github.com/devopsfaith/krakend/router/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // Namespace is the key to look for extra configuration details
 const Namespace = "github_com/jordelca/krakend-opa"
 
-// NewLogger returns a krakend logger wrapping a gologging logger
-func NewLogger(cfg config.ExtraConfig, ws ...io.Writer) {
-	log.Println("Hello world!")
+func HandlerFactory(hf ginkrakend.HandlerFactory, logger logging.Logger) ginkrakend.HandlerFactory {
+	logger.Info("OPA: HandlerFactory")
+	return OpaValidator(hf, logger)
+}
+
+func OpaValidator(hf ginkrakend.HandlerFactory, logger logging.Logger) ginkrakend.HandlerFactory {
+	return func(cfg *config.EndpointConfig, prxy proxy.Proxy) gin.HandlerFunc {
+		handler := hf(cfg, prxy)
+		logger.Info("OPA: validator enabled for the endpoint")
+
+		return func(c *gin.Context) {
+			if rand.Int()%2 == 0 {
+				logger.Info("OPA: Unauthorized")
+				c.AbortWithError(http.StatusUnauthorized, errors.New("error"))
+				return
+			}
+
+			logger.Info("OPA: Authorized")
+			handler(c)
+		}
+	}
 }
