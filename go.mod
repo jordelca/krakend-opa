edc4ba1a68cc95c8928ee574afbf59d887e3e983
Jorge Delgado Carrasco <jorge.delgado@scalefast.com>
2021-05-21 10:33:40 +0200

    Middleware

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jordelca/krakend-opa
+
+go 1.15
+
+require (
+	github.com/devopsfaith/krakend v1.3.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.7.1
+	github.com/open-policy-agent/opa v0.28.0
+)
--- opa.go
+++ opa.go
@@ -1,15 +1,18 @@
 package opa
 
 import (
-	"errors"
-	"math/rand"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/devopsfaith/krakend/config"
 	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
 	ginkrakend "github.com/devopsfaith/krakend/router/gin"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/rego"
 )
 
 // Namespace is the key to look for extra configuration details
@@ -25,9 +28,63 @@ func OpaValidator(hf ginkrakend.HandlerFactory, logger logging.Logger) ginkraken
 		logger.Info("OPA: validator enabled for the endpoint", cfg.Endpoint)
 
 		return func(c *gin.Context) {
-			if rand.Int()%2 == 0 {
+
+			receivedToken := strings.TrimPrefix(c.Request.Header.Get("Authorization"), "Bearer ")
+
+			parsedToken, err := jwt.Parse(receivedToken, nil)
+			if err != nil || parsedToken == nil {
+				fmt.Println("token is invalid: ", err)
+			}
+
+			claims := parsedToken.Claims.(jwt.MapClaims)
+
+			// Define a simple policy.
+			module := `
+				package example
+
+				default allow = false
+
+				allow {
+					input.role = "admin"
+				}
+			`
+
+			// Compile the module. The keys are used as identifiers in error messages.
+			compiler, _ := ast.CompileModules(map[string]string{
+				"example.rego": module,
+			})
+
+			// Create a new query that uses the compiled policy from above.
+			rego := rego.New(
+				rego.Query("data.example.allow"),
+				rego.Compiler(compiler),
+				rego.Input(
+					map[string]interface{}{
+						"role":   claims["role"],
+						"method": c.Request.Method,
+					},
+				),
+			)
+
+			// Run evaluation.
+			rs, err := rego.Eval(c)
+
+			if err != nil {
+				// Handle error.
+				logger.Info("OPA: Error")
+			}
+
+			// Inspect results.
+			fmt.Println("len:", len(rs))
+			fmt.Println("user", c.Param("user"))
+			fmt.Println("bearer", c.Request.Header.Get("Authorization"))
+			fmt.Println("method", c.Request.Method)
+			fmt.Println("value:", rs[0].Expressions[0].Value)
+
+			if rs[0].Expressions[0].Value == false {
 				logger.Info("OPA: Unauthorized")
-				c.AbortWithError(http.StatusUnauthorized, errors.New("error"))
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid role"})
+				c.Abort()
 				return
 			}
 
